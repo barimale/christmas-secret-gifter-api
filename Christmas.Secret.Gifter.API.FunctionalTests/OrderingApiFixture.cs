938337ae4a49fcc55f752c0077f9bd356a70e630
Mateusz <mateusz.wolnica@gmail.com>
2024-07-31 17:53:19 +0200

    .

---
+++ Christmas.Secret.Gifter.API.FunctionalTests/OrderingApiFixture.cs
@@ -0,0 +1,75 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Aspire.Hosting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace eShop.Ordering.FunctionalTests;
+
+public sealed class OrderingApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly IHost _app;
+
+    public IResourceBuilder<PostgresServerResource> Postgres { get; private set; }
+
+    private string _postgresConnectionString;
+
+    public OrderingApiFixture()
+    {
+        var options = new DistributedApplicationOptions { AssemblyName = typeof(OrderingApiFixture).Assembly.FullName, DisableDashboard = true };
+        var appBuilder = DistributedApplication.CreateBuilder(options);
+        Postgres = appBuilder.AddPostgres("OrderingDB");
+        _app = appBuilder.Build();
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(config =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { $"ConnectionStrings:{Postgres.Resource.Name}", _postgresConnectionString },
+            });
+        });
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<IStartupFilter>(new AutoAuthorizeStartupFilter());
+        });
+        return base.CreateHost(builder);
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await base.DisposeAsync();
+        await _app.StopAsync();
+        if (_app is IAsyncDisposable asyncDisposable)
+        {
+            await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+        }
+        else
+        {
+            _app.Dispose();
+        }
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _app.StartAsync();
+        _postgresConnectionString = await Postgres.Resource.GetConnectionStringAsync();
+    }
+
+    private class AutoAuthorizeStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder =>
+            {
+                builder.UseMiddleware<AutoAuthorizeMiddleware>();
+                next(builder);
+            };
+        }
+    }
+}
